955f4b86406f4fc6b216b8821c47027b8b331128
unknown <idil.iman1999@gmail.com>
2023-03-06 12:06:26 +0800

    update env

--- database/conn.js
+++ database/conn.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const MONGO_URI =
-  "mongodb+srv://admin:abc12345@cluster0.soub0b9.mongodb.net/?retryWrites=true&w=majority";
+  'mongodb+srv://admin:Abc12345@cluster0.soub0b9.mongodb.net/?retryWrites=true&w=majority';
 
 const connectMongo = async () => {
   try {
     const { connection } = await mongoose.connect(MONGO_URI);
     if (connection.readyState === 1) {
-      console.log("Databse Connected");
+      console.log('Databse Connected');
     }
   } catch (error) {
     return Promise.reject(error);
